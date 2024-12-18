eb5a30a35c740e9796a1457aab8d254a4d5dd9be
sheeba-khan <124671004+sheebak-Equitec@users.noreply.github.com>
2024-12-18 12:41:35 +0530

    Initial commit

---
+++ BBK.HRMS.Reports.Library/CrystalReportToPDF.cs
@@ -0,0 +1,71 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace YourNamespace
+{
+    public class CrystalReportToPDF
+    {
+        private readonly List<object> reportParameters;
+        private readonly string connectionString;
+        private readonly string reportPath;
+
+        public CrystalReportToPDF(string reportPath, List<object> reportParameters, string connectionString)
+        {
+            this.connectionString = connectionString;
+            this.reportPath = reportPath;
+            this.reportParameters = reportParameters;
+        }
+
+        public void CreateReportPDF()
+        {
+            try
+            {
+                ReportDocument reportDocument = new ReportDocument();
+                reportDocument.Load(reportPath);
+
+                reportDocument.Database.Tables[0].ApplyLogOnInfo(LogonInfo());
+
+                for (int i = 0; i < reportParameters.Count; i++)
+                {
+                    string parameterName = reportDocument.ParameterFields[i].Name;
+                    reportDocument.SetParameterValue(parameterName, reportParameters[i]);
+                }
+
+                Stream stream = reportDocument.ExportToStream(ExportFormatType.PortableDocFormat);
+                byte[] bytes;
+                using (var memoryStream = new MemoryStream())
+                {
+                    stream.CopyTo(memoryStream);
+                    bytes = memoryStream.ToArray();
+                }
+
+                string base64EncodedString = Convert.ToBase64String(bytes);
+                Console.Write(base64EncodedString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        private TableLogOnInfo LogonInfo()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
+            TableLogOnInfo crLogonInfo = new TableLogOnInfo
+            {
+                ConnectionInfo =
+                {
+                    ServerName = builder.DataSource,
+                    DatabaseName = builder.InitialCatalog,
+                    UserID = builder.UserID,
+                    Password = builder.Password
+                }
+            };
+            return crLogonInfo;
+        }
+    }
+}
---
+++ BBK.HRMS.Reports/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using YourNamespace;
+
+namespace BBK.HRMS.Reports
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> reportParameters;
+            string reportName;
+            reportName = "C:\\Users\\Equilap41\\Documents\\GitHub\\HRMS\\BBK-HRMS-MVC\\HRMS\\Reports\\M5\\rptM5_ListMnthDistribution.rpt";
+            string connectionString = "server=.;Database=BBKDB;User Id=sa;Password=123;TrustServerCertificate=True";
+            
+            reportParameters = new List<object>();
+            reportParameters.Add("BBK");
+            reportParameters.Add("");
+            reportParameters.Add("");
+            reportParameters.Add("");
+            reportParameters.Add("");
+            reportParameters.Add("1763");
+            reportParameters.Add("01/01/2021");
+            reportParameters.Add("");
+            reportParameters.Add("1763");
+            reportParameters.Add("A");
+            try
+            {
+                CrystalReportToPDF crystalReportToPDF = new CrystalReportToPDF(reportName, reportParameters, connectionString);
+                crystalReportToPDF.CreateReportPDF();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                Console.WriteLine("Stack Trace: " + ex.StackTrace);
+                Console.WriteLine("Press Enter to exit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
